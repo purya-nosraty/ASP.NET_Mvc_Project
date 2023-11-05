7e979ff061ec88b90c7f8ed663bd96a707054e70
poorya Nosraty <purya.nosraty@gmail.com>
2023-11-05 16:08:29 +0330

    11/05/2023

---
+++ Data/DatabaseContex/DatabaseContex.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace DataLayer
+{
+	public class DatabaseContex : DbContext
+	{
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		public DatabaseContex() : base()
+		{
+		}
+
+		public DbSet<Page> Pages { get; set; }
+		public DbSet<PageGroup> PageGroups { get; set; }
+		public DbSet<PageComment> PageComments { get; set; }
+	}
+}
